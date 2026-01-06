ba14a0a09204a4c40fec4b02a242a0d68dbc63dd
Jeyagayathre <gayathrejeya@gmail.com>
2026-01-06 16:30:19 +0530

    Update app.js

--- frontend/frontend/app.js
+++ frontend/frontend/app.js
@@ -1,5 +1,5 @@
 async function login() {
-  const res = await fetch("http://localhost:3000/login", {
+const res = await fetch("http://localhost:3000/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
