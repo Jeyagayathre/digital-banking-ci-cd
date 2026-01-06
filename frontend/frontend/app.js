eaf8c687260c8d85cb95760f16f977287acf2fd4
Jeyagayathre <gayathrejeya@gmail.com>
2026-01-06 16:28:12 +0530

    Create app.js

---
+++ frontend/frontend/app.js
@@ -0,0 +1,13 @@
+async function login() {
+  const res = await fetch("http://localhost:3000/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: document.getElementById("username").value,
+      password: document.getElementById("password").value
+    })
+  });
+
+  const data = await res.json();
+  document.getElementById("result").innerText = data.message;
+}
